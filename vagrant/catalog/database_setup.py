c3c21a47db66b1e8ab7e1078fb014686d542c3f1
Islam Salah CM I.salah RM Engineer <i.salah@egyptianbanks.net>
2020-01-09 21:23:05 +0200

    update API Endpoint and html pages

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,3 +1,4 @@
+import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -5,59 +6,49 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-
 class User(Base):
-    __tablename__ = 'user'
+	__tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    picture = Column(String(250))
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+	picture = Column(String(250))
 
+class Category(Base):
+	__tablename__ = 'category'
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+	id = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable = False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+	@property
+	def serialize(self):
+	    """Return object data in easily serializeable format"""
+	    return {
+	    	'id': self.id,
+	        'name': self.name,
+	    }
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'name': self.name,
-            'id': self.id,
-        }
+class CategoryItem(Base):
+	__tablename__ = 'category_item'
 
+	name = Column(String(80), nullable = False)
+	description = Column(String(250))
+	id = Column(Integer, primary_key = True)
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
+	@property
+	def serialize(self):
+	    """Return object data in easily serializeable format"""
+	    return {
+	        'category': self.category.name,
+	        'description': self.description,
+	        'name': self.name,
+	    }
 
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-    description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'name': self.name,
-            'description': self.description,
-            'id': self.id,
-            'price': self.price,
-            'course': self.course,
-        }
-
-
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
-
-
+engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.create_all(engine)
